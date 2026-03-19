4cd5398cc6160045c69095b438cdc2a245cb6502
Adhirajkadu <neelamkadu18@gmail.com>
2026-03-19 21:14:11 +0530

    Add files via upload

---
+++ intersection.py
@@ -0,0 +1,8 @@
+setx={"blue","green"}
+sety={"yellow","blue"}
+print("Original set elements are :")
+print(setx)
+print(sety)
+print("Intersection of two sets are :")
+setz = setx.intersection(sety)
+print(setz)
---
+++ opperations.py
@@ -0,0 +1,16 @@
+Sets={1,2,3}
+print(Sets)
+
+my_set={1,2,"Hello",3.3,2}
+print(my_set)
+
+my_set={1,2,3,4,4,3,5,6,3,2,2}
+print(my_set)
+
+my_set=set([1.0,"Hello",1.45,2.6])
+print(my_set,"\n")
+
+O=set([0,1,2,3,4,5])
+print("Original set :",O)
+O.pop()
+print("After removing First elements from the sets",O)
